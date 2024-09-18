bf2cdb779d9c31f6d50b27f59587b4dc8112f626
Ethan Yen <ethan.yen30@gmail.com>
2024-09-18 10:02:48 -0400

    training model and replay buffer

---
+++ replay_buffer.py
@@ -0,0 +1,19 @@
+from collections import deque
+import random
+import numpy as np
+
+
+class ReplayBuffer:
+    def __init__(self, capacity):
+        self.buffer = deque(maxlen=capacity)
+    
+    def add(self, state, action, reward, next_state, done):
+        self.buffer.append((state, action, reward, next_state, done))
+    
+    def sample(self, batch_size):
+        batch = random.sample(self.buffer, batch_size)
+        states, actions, rewards, next_states, dones = zip(*batch)
+        return np.stack(states), actions, rewards, np.stack(next_states), dones
+    
+    def __len__(self):
+        return len(self.buffer)
