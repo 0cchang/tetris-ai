c087693a86198e083b45744945c98be81beb43a0
0cchang <calebschang13@gmail.com>
2024-09-26 17:14:34 -0400

    idk

--- caleb/tetris-gym.py
+++ caleb/tetris-gym.py
@@ -2,7 +2,7 @@ import cv2
 import gymnasium as gym
 
 #our input vector is [x change -4 to 5 (negatives move left, positive move right), rotate 0 to 3 (rotate CC) or negative (rotate clockwise)],  
-vectors = [[4,0]]
+vectors = [[2,0],[-4,0]]
 
 from tetris_gymnasium.envs.tetris import Tetris
 
@@ -11,19 +11,12 @@ if __name__ == "__main__":
     env.reset(seed=42)
     i = 0
     terminated = False
-    while not terminated:
-        env.render()
-        
-        moveX, rotate = vectors[i % 1]
-        if moveX < 0:
-            for i in range(abs(moveX)):
-                action = env.unwrapped.actions.move_left
-                observation, reward, terminated, truncated, info = env.step(action)
-        else:
-            for i in range(moveX):
-                action = env.unwrapped.actions.move_right
-                observation, reward, terminated, truncated, info = env.step(action)
-        i += 1
+    action = env.unwrapped.actions.move_left
+    env.render()
+    observation, reward, terminated, truncated, info = env.step(action)
+    
+    
+    print(observation)
 
-        key = cv2.waitKey(100) # timeout to see the movement
+    key = cv2.waitKey(0) # timeout to see the movement
     print("Game Over!")
