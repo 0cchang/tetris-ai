65afe6ceaa2d69be354c594bd8e385db15b3e85a
0cchang <calebschang13@gmail.com>
2024-09-28 14:57:03 -0400

    updated training model

--- caleb/training_model.py
+++ caleb/training_model.py
@@ -8,14 +8,17 @@ CURR_PIECE_SIZE = 2*4
 NEXT_PIECE_SIZE = 2*4
 
 
-# DQN class responsible for forward propogation
-class DQN(nn.modules):
+import torch
+import torch.nn as nn
+
+# DQN class responsible for forward propagation
+class DQN(nn.Module):  # Changed from nn.modules to nn.Module
     
     def __init__(self, input_dim, output_dim):
         super(DQN, self).__init__()
 
         # Creates the input layer connection from the dimensions of the input to 
-        # 128 neurons in the hidden layer (good practice to use this i guess)
+        # 128 neurons in the hidden layer
         self.input_layer = nn.Linear(input_dim, 128)
 
         # One hidden layer
